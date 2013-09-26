883d8386f0fb15a36b0c7559bf885cfd750b2735
ViniciusAtaide <viniciusataid@gmail.com>
2013-09-26 09:14:26 -0300

    add all project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorld::Application.config.secret_key_base = '00d7823ab023415af948a972156747db0b0d0b65ab85988a10c78fc654351adc3597e0badb55420c7722832ad6a94b8f6c4ddc946977f068b7c694fc279981bd'
